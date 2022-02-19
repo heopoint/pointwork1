d0e39b02118d29f59ac86aa30d5391bba033cddf
heopoint <heopoint@naver.com>
2022-02-20 02:01:26 +0900

    'ok4'

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import { create, router as _router, defaults, rewriter } from "json-server";
-const server = create();
-const router = _router("./db.json");
-const middlewares = defaults({
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
   static: "./build",
 });
 const port = process.env.PORT ||5000;
 
 server.use(middlewares);
 server.use(
-  rewriter({
+  jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
